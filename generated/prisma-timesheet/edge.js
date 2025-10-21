df569b4a261cdc2aaed6e37471ea5cb69c37b075
Tech Lead - Light <thanat.light@schoolbright.co>
2025-10-21 10:04:25 +0700

    ✅ chore: remove 'postinstall' flag from configuration in generated Prisma files

--- generated/prisma-timesheet/edge.js
+++ generated/prisma-timesheet/edge.js
@@ -232,7 +232,6 @@ const config = {
     "timesheet"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "timesheet": {
       "url": {
--- generated/prisma-timesheet/index.js
+++ generated/prisma-timesheet/index.js
@@ -233,7 +233,6 @@ const config = {
     "timesheet"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "timesheet": {
       "url": {
