54f5c625f6ecd628ae1ed0b7ebba746de9d0e98a
Tech Lead - Light <thanat.light@schoolbright.co>
2025-10-24 10:19:01 +0700

    ปรับปรุงการเรียกใช้งาน API ในฟังก์ชัน CallAPI โดยเพิ่มการตั้งค่า headers สำหรับ Content-Type เป็น multipart/form-data

--- src/stores/actions/authentication/sign-in/action.ts
+++ src/stores/actions/authentication/sign-in/action.ts
@@ -9,7 +9,12 @@ type SignInRequest = { username: string; password: string };
 export const CallAPI = createAsyncThunk(
   API_METHOD.POST + API_ENDPOINT,
   async (formData: FormData) => {
-    const response = await axios.post(API_ENDPOINT, formData);
+    const response = await axios.post(API_ENDPOINT, formData , {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    
+    });
     return response.data;
   }
 );
