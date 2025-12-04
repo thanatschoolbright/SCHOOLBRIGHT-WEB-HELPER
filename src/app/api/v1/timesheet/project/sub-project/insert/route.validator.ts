32ab39dfa678a80ad3c7a24eefe544c2f36df412
Tech Lead - Light <thanat.light@schoolbright.co>
2025-12-04 18:26:42 +0700

    Refactor Code

--- src/app/api/v1/timesheet/project/sub-project/insert/route.validator.ts
+++ src/app/api/v1/timesheet/project/sub-project/insert/route.validator.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const Schema = z.object({
   id: z.union([z.number().min(1), z.string().min(1).optional()]),
   name: z.string().min(1),
+  name_en: z.string().optional(),
   project_id: z.union([z.number().min(1), z.string().min(1)]),
   by: z.union([z.number().min(1), z.string().min(1)]),
   backlogDescription: z.any().optional(),
