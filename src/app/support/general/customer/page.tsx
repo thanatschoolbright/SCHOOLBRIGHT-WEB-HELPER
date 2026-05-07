a5b4711ce7e053f4639d356f1bf094f33ecf0a26
Tech Lead - Light <thanat.light@schoolbright.co>
2026-05-07 10:30:16 +0700

    ♻️ ระบบ ServerStatusPage : ลบการเรียกใช้งาน fetchServerStatus ที่ไม่จำเป็น (หน้าที่แก้ไข src/app/support/general/customer/page.tsx)

--- src/app/support/general/customer/page.tsx
+++ src/app/support/general/customer/page.tsx
@@ -4,18 +4,11 @@ import DashboardLayout from "@components/layouts/backend-layout";
 import dayjs from "dayjs";
 import "dayjs/locale/th";
 import buddhistEra from "dayjs/plugin/buddhistEra";
-import { useEffect } from "react";
 
 dayjs.extend(buddhistEra);
 dayjs.locale("th");
 
 export default function ServerStatusPage() {
-  const { fetchServerStatus } = useServerStatusStore();
-
-  useEffect(() => {
-    fetchServerStatus("normal");
-  }, [fetchServerStatus]);
-
   return (
     <DashboardLayout>
       <></>
